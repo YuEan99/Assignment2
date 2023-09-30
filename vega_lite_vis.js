e4a8a93a703089376ef3d000b93e6ed249a60746
YuEan99 <126786457+YuEan99@users.noreply.github.com>
2023-09-30 18:00:14 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,33 @@
+var vg_1 = "line_chart.json";  // Location of the vegalite JSON file
+var vg_2 = "bump_chart2.json";  // Location of the vegalite JSON file
+var vg_3 = "area_chart.json";  // Location of the vegalite JSON file
+var vg_4 = "choropleth_map.json";  // Location of the vegalite JSON file
+var vg_5 = "proportional_symbol.json";  // Location of the vegalite JSON file
+var vg_6 = "scatter_plot2.json";  // Location of the vegalite JSON file
+var vg_7 = "small_multiples.json";  // Location of the vegalite JSON file
+var vg_8 = "donut_chart.json";  // Location of the vegalite JSON file
+
+vegaEmbed("#line_chart", vg_1, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#bump_chart", vg_2, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#area_chart", vg_3, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#choropleth_map", vg_4, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#proprotional_symbol", vg_5, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#scatter_plot", vg_6, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#small_multiples", vg_7, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#donut_chart", vg_8, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
